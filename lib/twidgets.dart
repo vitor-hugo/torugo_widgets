77e461f5a54fa6895deb9d97057e153c5a991b0a
vitor-hugo <vitor83dev@gmail.com>
2026-08-28 10:37:17 -0300

    init: initial library exports

---
+++ lib/twidgets.dart
@@ -0,0 +1,22 @@
+// Copyright (c) torugo.com.br
+// Licensed under the MIT License.
+
+library;
+
+//
+// COMMON
+//
+export 'common/t_adaptive_shapes.dart';
+export 'common/t_shadow_painter.dart';
+
+//
+// UTILS
+//
+export 'utils/t_platform.dart';
+
+//
+// WIDGETS
+//
+
+// CONTAINERS
+export 'widgets/containers/t_container.dart';
